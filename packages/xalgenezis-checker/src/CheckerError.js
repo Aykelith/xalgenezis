c357c3522e199d7e3436b654776fcceff0b2f7c2
AlexxanderX <zalexxanderx@gmail.com>
2021-01-04 19:22:07 +0200

    xalgenezis-checker: Fixed CheckerError and pushed version to 1.0.1

--- packages/xalgenezis-checker/src/CheckerError.js
+++ packages/xalgenezis-checker/src/CheckerError.js
@@ -1,15 +1,21 @@
+//= Functions & Modules
+// xalgenezis
 import GenezisGeneralError from "@aykelith/xalgenezis-generalerror";
 
 export default class extends GenezisGeneralError {
-    constructor(type, data, originalError, customMessage) {
-        super(customMessage || `genezis/GeneralError thrown`);
+    constructor(type, property, value, originalError, additionalData) {
+        super(
+            type,
+            {
+                property,
+                value,
+                additionalData
+            },
+            originalError, 
+            `genezis/CheckerError of type ${type} on "${property}":"${value}"`
+        );
 
         this.name = this.constructor.name;
-
-        this.type = type;
-        this.data = data;
-        this.originalError = originalError;
-
         Error.captureStackTrace(this, this.constructor);
     }
 }
