9de29764917818179999190bacc373f1b5b9bec0
AlexxanderX <zalexxanderx@gmail.com>
2021-02-23 18:13:13 +0000

    expressjs,mongodb: Maked genericerrors as peer dependency

--- packages/xalgenezis-expressjs/src/RequestWithStatusUtils.ts
+++ packages/xalgenezis-expressjs/src/RequestWithStatusUtils.ts
@@ -1,6 +1,7 @@
 export default {
-    createStatusSession: global._genezis_router.requestWithStatus.createStatusSession,
-    setStatusSession: global._genezis_router.requestWithStatus.setStatusSession,
-    getStatusSession: global._genezis_router.requestWithStatus.getStatusSession,
-    deleteStatusSession: global._genezis_router.requestWithStatus.deleteStatusSession
-}
+  //createStatusSession: global._genezis_router.requestWithStatus.createStatusSession,
+  //setStatusSession: global._genezis_router.requestWithStatus.setStatusSession,
+  //getStatusSession: global._genezis_router.requestWithStatus.getStatusSession,
+  //deleteStatusSession: global._genezis_router.requestWithStatus.deleteStatusSession
+};
+
